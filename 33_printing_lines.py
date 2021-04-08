4df772992972a99d17b9312d007a3a7b6a0b4465
Shobha Dhingra <shobhadhingra14@gmail.com>
2021-04-08 16:15:44 +0530

    Class 7

---
+++ 33_printing_lines.py
@@ -0,0 +1,13 @@
+def print_line():
+    for i in range(10):
+        print("_ ", end="")
+    print()
+
+print("/nthe end")
+
+
+
+"""
+/n- new line character
+\t- tab
+"""
---
+++ 34_printing_lines.py
@@ -0,0 +1,6 @@
+def print_line():
+    for i in range(10):
+        print("_ ", end="")
+    print()
+
+print("/nthe end")
