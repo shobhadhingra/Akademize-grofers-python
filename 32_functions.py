769c9b1fc6ff1ce03926b98d3de76f8d20d06a54
Shobha Dhingra <shobhadhingra14@gmail.com>
2021-04-08 16:16:31 +0530

    Class 6

---
+++ 32_functions.py
@@ -0,0 +1,17 @@
+def foo():
+    print("foo")
+
+def bar():
+    print("bar")
+
+def main():
+        print("I am main")
+        foo()
+        bar()
+        print("end of main")
+
+ 
+    print("hello function")
+    main()
+    print("Bye Function")
+
