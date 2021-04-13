4724914f0dd49e8bc287eac4018fea13bd571c46
Shobha Dhingra <shobhadhingra14@gmail.com>
2021-04-13 18:52:41 +0530

    class 7

--- 35_starlines.py
+++ 35_starlines.py
@@ -1,10 +1,13 @@
 def print_star_triangle(n):
-    for i in range():
+    for i in range(n):
         print("*", end="")
     print()
 
-
 def main():
- for i in range(5):
-     print_star_triangle(i+5)
+    for i in range(5):
+        print_star_triangle(i+1)
+
+
+main()
+        
 
